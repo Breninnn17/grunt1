350acf7587f3d7f48556a7490729f9ae483c9212
Breno Neves <Brenonevessantos2014@hotmail.com>
2024-03-11 18:18:23 -0300

    2

--- Gruntfile.js
+++ Gruntfile.js
@@ -3,11 +3,11 @@ module.exports = function(grunt) {
         pkg: grunt.file.readJSON('package.json'),
         less: {
             development: {
-                files:{
+                files: {
                     'dev/styles/main.css': 'src/styles/main.less'
                 }
             },
-            production:{
+            production: {
                 options: {
                     compress: true,
                 },
@@ -18,8 +18,12 @@ module.exports = function(grunt) {
         },
         watch: {
             less: {
-                files:['src/styles/**/*.less'],
+                files: ['src/styles/**/*.less'],
                 tasks: ['less:development']
+            },
+            html: {
+                files: ['src/index.html'],
+                tasks: ['replace:dev']
             }
         },
         replace: {
@@ -29,6 +33,10 @@ module.exports = function(grunt) {
                         {
                             match: 'ENDERECO_DO_CSS',
                             replacement: './styles/main.css'
+                        },
+                        {
+                            match: 'ENDERECO_DO_JS',
+                            replacement: '../src/scripts/main.js'
                         }
                     ]
                 },
@@ -36,31 +44,62 @@ module.exports = function(grunt) {
                     {
                         expand: true,
                         flatten: true,
-                        src: ['/src/index.html'],
+                        src: ['src/index.html'],
                         dest: 'dev/'
+                    }
+                ]
+            },
+            dist: {
+                options: {
+                    patterns: [
+                        {
+                            match: 'ENDERECO_DO_CSS',
+                            replacement: './styles/main.min.css'
+                        },
+                        {
+                            match: 'ENDERECO_DO_JS',
+                            replacement: './scripts/main.min.js'
+                        }
+                    ]
+                },
+                files: [
+                    {
+                        expand: true,
+                        flatten: true,
+                        src: ['prebuild/index.html'],
+                        dest: 'dist/'
                     }
                 ]
             }
         },
-        htmlmin:{
+        htmlmin: {
             dist: {
-                options:{
+                options: {
                     removeComments: true,
-                    collapseWhitespace: true,
+                    collapseWhitespace: true
                 },
                 files: {
-                    'prebuild/index.html' : "src/index.html"
+                    'prebuild/index.html': 'src/index.html'
+                }
+            }
+        },
+        clean: ['prebuild'],
+        uglify: {
+            target: {
+                files: {
+                    'dist/scripts/main.min.js': 'src/scripts/main.js'
                 }
             }
         }
     })
 
-        grunt.loadNpmTasks('grunt-contrib-less');
-        grunt.loadNpmTasks('grunt-contrib-watch');
-        grunt.loadNpmTasks('grunt-replace');
-        grunt.loadNpmTasks('grunt-contrib-htmlmin');
+    grunt.loadNpmTasks('grunt-contrib-less');
+    grunt.loadNpmTasks('grunt-contrib-watch');
+    grunt.loadNpmTasks('grunt-replace');
+    grunt.loadNpmTasks('grunt-contrib-htmlmin');
+    grunt.loadNpmTasks('grunt-contrib-clean');
+    grunt.loadNpmTasks('grunt-contrib-uglify');
 
-
-        grunt.registerTask('default', ['watch']);
-        grunt.registerTask('build', ['less:production']);
+    grunt.registerTask('default', ['watch']);
+    grunt.registerTask('build', ['less:production', 'htmlmin:dist', 'replace:dist', 'clean', 'uglify']);
 }
